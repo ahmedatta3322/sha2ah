c5c05c6ac1d123e71b7f45bb13e191fa3b9e589b
ahmed atta <ahmedatta3322@gmail.com>
2021-06-25 20:22:32 +0200

    first commit

---
+++ sha2ah/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for sha2ah project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sha2ah.settings')
+
+application = get_asgi_application()
---
+++ utilities/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.db.models.base import Model
+
+# Create your models here.
+class City(models.Model):
+    pass
+class Country(models.Model):
+    pass
+class Government(models.Model):
+    pass
+class Address(models.Model)
