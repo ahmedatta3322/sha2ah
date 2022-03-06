4dd0f4a92cca4366ccae02637ad396f41e775707
ahmed atta <ahmedatta3322@gmail.com>
2022-03-06 23:48:27 +0200

    fix routing

--- estate/urls.py
+++ estate/urls.py
@@ -4,9 +4,8 @@ from rest_framework import routers
 from estate.views import EstateViewSet, UnitViewSet
 
 router = routers.SimpleRouter()
-router.register(r"units", UnitViewSet)
-
-router.register(r'estates', EstateViewSet)
+# router.register(r"units", UnitViewSet)
+router.register(r"", EstateViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
