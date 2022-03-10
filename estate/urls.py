a21722722a009080b22959ec9016182a26568633
ahmed atta <ahmedatta3322@gmail.com>
2022-03-11 01:21:18 +0200

    fix trailing slash

--- estate/urls.py
+++ estate/urls.py
@@ -3,7 +3,7 @@ from rest_framework import routers
 
 from estate.views import EstateViewSet, UnitViewSet,EstateTypesViewSet
 
-router = routers.SimpleRouter()
+router = routers.SimpleRouter(trailing_slash=False)
 router.register(r"units", UnitViewSet)
 router.register(r"estates", EstateViewSet)
 router.register(r"estatetypes", EstateTypesViewSet)
